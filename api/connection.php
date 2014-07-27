7d58138e0687227d1e9d849511f56d3d8bcf021e
root <root@li637-60.members.linode.com>
2014-07-27 22:17:41 +0800

    use private folder, make sure to deny it at server conf

--- api/connection.php
+++ api/connection.php
@@ -6,7 +6,7 @@
 //$mysql_db_password = "xxx";
 //$mysql_db_database = "ca_test";
 
-include('db.conf');
+include('../private/db.conf');
 
 mysql_connect($mysql_db_hostname, $mysql_db_user, $mysql_db_password) or DIE (mysql_error());
 mysql_select_db($mysql_db_database) or DIE (mysql_error());
