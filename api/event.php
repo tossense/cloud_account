356125ae4f1320a31bb1296d5ad1511117617198
TosSense <tossense@gmail.com>
2015-06-02 00:04:34 +0800

    improve backend check

--- api/event.php
+++ api/event.php
@@ -21,6 +21,24 @@ else
 }
 
 /**
+ * Checks if a string is a valid timestamp.
+ *
+ * @param  string $timestamp Timestamp to validate.
+ * 
+ * @return bool
+ */
+function is_timestamp($timestamp)
+{
+	$check = (is_int($timestamp) OR is_float($timestamp))
+		? $timestamp
+		: (string) (int) $timestamp;
+
+	return  ($check === $timestamp)
+			AND ( (int) $timestamp <=  PHP_INT_MAX)
+			AND ( (int) $timestamp >= ~PHP_INT_MAX);
+}
+
+/**
  * @param $eventJsonArray
  * @return array
  */
@@ -46,41 +64,46 @@ function addEvent($eventJsonArray)
 	}
 	$sqlArray = toSqlArray($eventJsonArray, $keyToColumn, $link);
 	$groupId = getGroupId($group, $link)[$group];
-	$sqlArray["groupId"] = $groupId;
-	if($ts)
-		$sqlArray["time"] = "FROM_UNIXTIME(".$ts.")";
 	if(!$groupId)
 	{
 		return retError($ret, "Invalid group name");
 	}
+
+	$users = array_keys($records);
+	$userNameIds = getUserId($users, $link);
+	if(count($userNameIds) != count($records))
+	{
+		$unknownUsers = array_diff($users, array_keys($userNameIds));
+		return retError($ret, "Unknown User: ".implode(", ", $unknownUsers));
+	}
+
+	foreach($userNameIds as $name => $userId)
+	{
+		$money = $records[$name];
+		if(!is_numeric($money))
+			return retError($ret, "Has Not Valid Number: ".$money);
+	}
+
+	$sqlArray["groupId"] = $groupId;
+	if($ts && is_timestamp($ts))
+		$sqlArray["time"] = "FROM_UNIXTIME(".$ts.")";
+
 	$sql = makeInsertSql("tbEvents", $sqlArray);
 	if( queryAndLogError($link, $sql, $ret) )
 	{
 		$eventId = $link->insert_id;
-		$users = array_keys($records);
-		$userNameIds = getUserId($users, $link);
-		if(count($userNameIds) == count($records))
+		$sqlRecords = "";
+		$sqlBalance = "";
+		foreach($userNameIds as $name => $userId)
 		{
-			$sqlRecords = "";
-			$sqlBalance = "";
-			foreach($userNameIds as $name => $userId)
-			{
-				$money = $records[$name];
-				if(!is_numeric($money))
-					return retError($ret, "Has Not Valid Number: ".$money);
-				$sqlRecords .= "INSERT INTO tbRecords (eventId, userId, money) VALUES ($eventId, $userId, $money);";
-				$sqlBalance .= "UPDATE tbGroupMembers SET balance=balance+($money) WHERE groupId=$groupId AND userId=$userId;";
-			}
-			$sql = $sqlRecords.$sqlBalance;
-			if(multiQueryAndLogError($link, $sql, $ret))
-			{
-				while ($link->next_result()) {;} // flush multi_queries
-			}
+			$money = $records[$name];
+			$sqlRecords .= "INSERT INTO tbRecords (eventId, userId, money) VALUES ($eventId, $userId, $money);";
+			$sqlBalance .= "UPDATE tbGroupMembers SET balance=balance+($money) WHERE groupId=$groupId AND userId=$userId;";
 		}
-		else
+		$sql = $sqlRecords.$sqlBalance;
+		if(multiQueryAndLogError($link, $sql, $ret))
 		{
-			$unknownUsers = array_diff($users, array_keys($userNameIds));
-			return retError($ret, "Unknown User: ".implode(", ", $unknownUsers));
+			while ($link->next_result()) {;} // flush multi_queries
 		}
 
 	}
